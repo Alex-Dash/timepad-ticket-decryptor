2fcf5c0e7ac363024ac10f91d3e703041707f904
Alex Dash <16655043+Alex-Dash@users.noreply.github.com>
2022-02-23 04:47:44 +0300

    Made the server script into its own proxy-API. UI is WIP. Retrieval of public keys is now possible using only the access code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,197 @@
+var DEF_PASSCODE = (document.cookie.includes("passcode=")) ? document.cookie.split('; ')
+    .find(row => row.startsWith('passcode=')).split("=")[1] : undefined
+
+var LAST_SCAN_TIMESTAMP = (document.cookie.includes("lscan=")) ? document.cookie.split('; ')
+    .find(row => row.startsWith('lscan=')).split("=")[1] : undefined
+
+var SERVER = (document.cookie.includes("srv=")) ? document.cookie.split('; ')
+    .find(row => row.startsWith('srv=')).split("=")[1] : undefined
+var META = {}
+var SYNCTABLE = {}
+
+
 function parseScanContents(scannedContent) {
     const cTrimmed = scannedContent.trim()
+    if (cTrimmed.length === 0) return {
+        type: "error",
+        content: undefined,
+        errText: "No data found"
+    }
 
     // Return early if the scanned code is a barcode
-    if(scannedContent.length<20){
-        return (!isNaN(parseInt(cTrimmed)))?{type:"ean-13", content:cTrimmed}:{type:"error", content:undefined}
+    if (cTrimmed.length < 20) {
+        return (!isNaN(parseInt(cTrimmed))) ? {
+            type: "ean-13",
+            content: cTrimmed
+        } : {
+            type: "error",
+            content: undefined,
+            errText: "Failed to parse the barcode"
+        }
     }
 
-    if(!cTrimmed.includes("https://") || !cTrimmed.includes("?d=")){
-        return {type:"error", content:undefined}
+    // Check if the scanned code is a URL
+    if (!cTrimmed.includes("https://") || !cTrimmed.includes("?d=")) {
+        return {
+            type: "error",
+            content: undefined,
+            errText: "Failed to find the URL"
+        }
     }
 
-    let encoded_shit = cTrimmed.substr(cTrimmed.lastIndexOf("?d=")+3)
+    let encoded_shit = cTrimmed.substr(cTrimmed.lastIndexOf("?d=") + 3)
     let ticket_type = cTrimmed.split("/")[4]
     try {
         encoded_shit = decodeURIComponent(encoded_shit)
         ticket_type = parseInt(ticket_type)
-      } catch(e) {
-        return {type:"error", content:undefined}
-      }
-    return {type:"qr", content:encoded_shit, ticket_type_id:ticket_type}
+    } catch (e) {
+        return {
+            type: "error",
+            content: undefined,
+            errText: "Failed to parse the URL"
+        }
+    }
+    return {
+        type: "qr",
+        content: encoded_shit,
+        ticket_type_id: ticket_type
+    }
 }
 
-
-function listenerSetup(){
+// listenerSetup sets up a listener on ENTER key, commonly used as "submit" on scanners
+function listenerSetup() {
     const area = document.getElementById("scanarea")
+    area.focus()
     area.addEventListener('keyup', (e) => {
         if (e.key === 'Enter') {
             console.log(parseScanContents(area.value))
             area.value = ""
-
         }
-      });
+    });
+}
+
+// apiRequest sends a custom request to the server
+async function apiRequest(payload, passcode = DEF_PASSCODE) {
+
+    if (!payload.Passcode && !passcode) {
+        return {
+            error: "No passcode provided"
+        }
+    }
+
+    if (!SERVER) {
+        return {
+            error: "No server configured"
+        }
+    }
+
+    if (!payload.Passcode) {
+        payload.Passcode = passcode
+    }
+
+    // Prepare request header vars  
+    const content_type = (typeof (payload) === 'object') ? "application/json; charset=UTF-8" : "application/x-www-form-urlencoded"
+
+
+    const response = await fetch(SERVER + '/req', {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin, // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': content_type
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: (typeof (payload) === 'object') ? JSON.stringify(payload) : payload
+    });
+    return response;
+
+}
+
+// config sets some global settings
+async function config(passcode, serverUrl) {
+    SERVER = serverUrl
+    document.cookie = `srv=${SERVER}; SameSite=None; Secure`;
+    let ret
+    await apiRequest({
+            "Handle": "/login",
+            "Passcode": passcode,
+            "Timestamp": `${parseInt(Date.now()/1000)}`
+        }).then((r) => r.text().then((t) => JSON.parse(t)))
+        .then((jso) => {
+            META = {}
+            META.valid = jso.code_valid
+            if (META.valid) {
+                document.cookie = `passcode=${passcode}; SameSite=None; Secure`;
+                // Map ticket_type_id to public key
+                m = new Map()
+                for (const entry of jso.ticket_types) {
+                    m.set(entry.re_id, entry.re_key)
+                }
+                META.ticket_types = m
+                ret = {
+                    success: true,
+                    error: undefined
+                }
+            } else {
+                ret = {
+                    success: false,
+                    error: "Failed to log in using the code"
+                }
+            }
+
+        })
+    return ret
+}
+
+function loginRoutine() {
+    if (DEF_PASSCODE) {
+        console.log("exists")
+        // Display logout button
+    } else {
+        // Display the login form
+        const loginForm = document.getElementById("login")
+        const btn = document.getElementById("login_btn")
+        const passInput = document.getElementById("passInput")
+        const srvInput = document.getElementById("srvInput")
+        const lErr = document.getElementById("l-err")
+        loginForm.style.display = "block"
+
+        // Bind login logic to a button
+        btn.onclick = function () {
+            let pass = passInput.value
+            let srv = srvInput.value
+            if (!pass || !srv) {
+                lErr.innerHTML = "Error: Fields cannot be blank"
+                return
+            }
+
+            try {
+                config(pass, srv).then(d => {
+                    if (!d.success) {
+                        lErr.innerHTML = d.error
+                        return
+                    } else {
+                        // login ok
+                        loginForm.style.display = "none"
+                    }
+                })
+
+            } catch (error) {
+                lErr.innerHTML = "Error: login info cannot be validated"
+                console.log(error)
+                return
+            }
+        }
+
+        // Clear any errors on input focus
+        passInput.onfocus = function () {
+            lErr.innerHTML = ""
+        }
+        srvInput.onfocus = function () {
+            lErr.innerHTML = ""
+        }
+
+    }
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,14 +1,73 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
+type ReqObj struct {
+	Handle    string
+	Passcode  string
+	Timestamp string
+	TicketId  string
+	TFrom     string
+	TTo       string
+}
+
+const chkUrl = "https://checkin.timepad.ru"
+
 func main() {
-	err := http.ListenAndServe(":3250", http.FileServer(http.Dir("../../assets")))
-	if err != nil {
-		fmt.Println("Failed to start server", err)
-		return
-	}
+
+	http.Handle("/", http.FileServer(http.Dir("../../assets")))
+
+	http.HandleFunc("/req", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			fmt.Fprint(w, "Not yet implemented")
+		case http.MethodPost:
+
+			// Parse input
+			var obj ReqObj
+			err := json.NewDecoder(r.Body).Decode(&obj)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Prepare proxy vars
+			data := url.Values{
+				"code":             {obj.Passcode},
+				"client_timestamp": {obj.Timestamp},
+			}
+
+			req, err := http.NewRequest("POST", chkUrl+obj.Handle, strings.NewReader(data.Encode()))
+
+			if err != nil {
+				log.Fatal(err)
+				fmt.Fprint(w, "{\"error\":\"Failed to POST to timepad\"}")
+				return
+			}
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			resp, err := http.DefaultClient.Do(req)
+
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(resp.Body)
+			fmt.Fprint(w, buf.String())
+
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":3250", nil))
+
+}
+
+func apiReq() {
+
 }
